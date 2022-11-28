cb69f22b1006ba391f3c7765d65f2fce36d70656
Devid Mazeeta Gade Francis <gdevid@athenahealth.com>
2022-11-28 08:55:42 +0530

    Hello World edited

--- introduction/hello_1.py
+++ introduction/hello_1.py
@@ -1,6 +1,15 @@
 # 'def' keyword is used to define the function
-def main():
+def add():
+	num1 = 10
+	num2 = 10
+	num3 = num1 + num2
+	return num3
+
+def printtext():
 	print("Hello World") # this statement prints only when this function is called
 
 print("Welcome to Python")
-main()
+printtext()
+result = add() # result = 30
+print(result)
+print(add())
