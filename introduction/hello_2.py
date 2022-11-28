164fbeaaceb7a36540b3edd99c5bc41a4e3216b2
Devid Mazeeta Gade Francis <gdevid@athenahealth.com>
2022-11-28 09:23:47 +0530

    Import and command line arguments'

--- introduction/hello_2.py
+++ introduction/hello_2.py
@@ -1,10 +1,12 @@
 import sys
 
-def print_text(name):
-	print("Hello World " + name, end=". ")
+def printtext(name):
+	print("Hello World", name, sep=" - ")
+	print("Hello World " + name, end=".")
 
 # predefined main function
-if __name__ == "__main__":
-	print_text(sys.argv[1]) # 'sys.argv[]' used to get first command line arguments
+if __name__ == '__main__':
+	printtext("Besant")
+	printtext(sys.argv[1]) # 'sys.argv[]' used to get first command line arguments
 
 print("Welcome to Python")
