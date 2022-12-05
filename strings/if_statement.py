e78bd2ca3225fbf8d6230eb30c36fe90659d163b
Devid Mazeeta Gade Francis <gdevid@athenahealth.com>
2022-12-05 13:11:52 +0530

    Removed unwanted spaces

--- strings/if_statement.py
+++ strings/if_statement.py
@@ -4,21 +4,21 @@ num3 = 200
 
 # method 1
 if num1 == num2:
-	print ("'num1' equal to 'num2'")
+	print("'num1' equal to 'num2'")
 
 	if num1 > num3:
-		print ("'num1' greater than 'num3")
+		print("'num1' greater than 'num3")
 	elif num3 > num2:
-		print ("'num3' greater than 'num2'")
+		print("'num3' greater than 'num2'")
 	else:
-		print ("prints when no statement matches")
+		print("prints when no statement matches")
 else:
-	print ("'num1' not equal to 'num2'")
+	print("'num1' not equal to 'num2'")
 
 # method 2
 if num1 == num2:
-	print ("'num1' equal to 'num2'")
-	if num1 > num3: print ("'num1' greater than 'num3")
-	elif num3 > num2: print ("'num3' greater than 'num2'")
-	else: print ("prints when no statement matches")
-else: print ("'num1' not equal to 'num2'")
+	print("'num1' equal to 'num2'")
+	if num1 > num3: print("'num1' greater than 'num3")
+	elif num3 > num2: print("'num3' greater than 'num2'")
+	else: print("prints when no statement matches")
+else: print("'num1' not equal to 'num2'")
--- strings/split_join.py
+++ strings/split_join.py
@@ -1,12 +1,16 @@
+# split => string to list
+# join => list to string
+
 name1 = ' oBjEcT OrIeNtEd LaNgUaGe '
-name2 = 'a'
+name2 = '-'
 name3 = ' '
 
 # list of substrings separated by the given delimiter
 print(name1.split(' ')) # list is returned using delimitting space
 
 # joins the elements in the given list together using the string as the delimiter
-print(name3.join(['', 'oBjEcT', 'OrIeNtEd', 'LaNgUaGe', ''])) # seperated using space
+texts = name1.split(' ') # ["", "oBjEcT", "OrIeNtEd", "LaNgUaGe", ""]
+print(name3.join(texts)) # seperated using space
 
 # joins the elements in the given list together using the string as the delimiter
-print(name3.join(['', 'oBjEcT', 'OrIeNtEd', 'LaNgUaGe', ''])) # seperated using space
+print(name2.join(['', 'oBjEcT', 'OrIeNtEd', 'LaNgUaGe', ''])) # seperated using space
