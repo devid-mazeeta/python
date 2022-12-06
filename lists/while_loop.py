b04cf21bc15aa6c033699ab1e65d51320e82a188
Devid Mazeeta Gade Francis <gdevid@athenahealth.com>
2022-12-06 09:24:16 +0530

    List concepts modified

--- lists/while_loop.py
+++ lists/while_loop.py
@@ -1,9 +1,17 @@
 # while loop
+
+# Example 1 -> Print 1 to 10
+num = 1
+
+while num < 11:
+	print(num)
+	num += 1
+
+# Example 2 -> 
 number = 1
 
 while number > 0:
-	number += 1
-	# number = number + 1
+	number += 1 # number = number + 1
 
 	if number == 5:
 		continue # jumps to the next iteration
@@ -11,4 +19,6 @@ while number > 0:
 	if number == 10:
 		break # ends the iteration of while loop
 
-	print (number)
+	print(number) # 2 3 4 6 7 8 9
+
+print("loop ends after break")
